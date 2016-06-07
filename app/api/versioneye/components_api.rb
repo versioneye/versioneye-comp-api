02c64bfcad59c04b620638d17acda8bb5282d8e4
reiz <robert.reiz.81@gmail.com>
2016-06-07 11:53:16 +0200

    Add more parameters for filtering.

--- app/api/versioneye/components_api.rb
+++ app/api/versioneye/components_api.rb
@@ -16,6 +16,8 @@ module Versioneye
       desc "fetch component by id"
       params do
         optional :api_key, type: String, desc: "API Key"
+        optional :language, type: String, desc: "Programming language"
+        optional :package_type, type: String, desc: "Package Manager"
       end
       get '/:component_id' do
         authorized?
@@ -23,6 +25,13 @@ module Versioneye
         comp_id  = params[:component_id].to_s
         comp_id  = comp_id.gsub(":", "/")
         products = Product.where(:prod_key => comp_id)
+        language = Product.decode_language( params[:language].to_s )
+        if language
+          products = products.where(:language => language)
+        end
+        if !params[:package_type].to_s.empty?
+          products = products.where(:prod_type => params[:package_type].to_s )
+        end
 
         components = comp_mapping( products )
         present components, with: EntitiesV2::ComponentEntity
