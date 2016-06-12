5eeebb9fb544af29060dbd03be70a2affc8787db
reiz <robert.reiz.81@gmail.com>
2016-06-12 16:36:40 +0200

    Add more tests.

--- spec/api/v2/component_api_spec.rb
+++ spec/api/v2/component_api_spec.rb
@@ -15,12 +15,36 @@ describe Versioneye::ComponentsApi, :type => :request do
     it "returns error code for not existing product" do
       product = ProductFactory.create_for_maven('junit', 'junit', '1.0.0')
       expect( product.save ).to be_truthy
+
+      repository = Repository.new({ :src => 'https://jcenter.bintray.com', :repotype => "Maven2" })
+      expect( product.repositories.push( repository ) )
+      expect( product.save )
+
+      license = License.new({
+        :language => product.language,
+        :prod_key => product.prod_key,
+        :version => '1.0.0',
+        :name => "MIT"  })
+      expect( license.save ).to be_truthy
+
       package_url =  "#{product_uri}/junit:junit?api_key=#{user_api.api_key}"
       get package_url
       expect( response.status ).to eql(200)
       json = JSON.parse response.body
       component = json.first
       expect( component['component_id'] ).to eq( 'junit:junit' )
+
+      expect( component['sources'].count ).to eq( 1 )
+      expect( component['sources'].first['name'] ).to eq( 'Bintray JCenter' )
+      expect( component['sources'].first['url'] ).to eq( 'https://jcenter.bintray.com' )
+      expect( component['sources'].first['repo_type'] ).to eq( 'Maven2' )
+      expect( component['sources'].first['updated'] ).to_not be_nil
+
+      expect( component['versions'].count ).to eq( 1 )
+      expect( component['versions'].count ).to eq( 1 )
+      expect( component['versions'].first['version'] ).to eq( '1.0.0' )
+      expect( component['versions'].first['licenses'].count ).to eq( 1 )
+      expect( component['versions'].first['licenses'].first ).to eq( 'MIT' )
     end
 
   end
